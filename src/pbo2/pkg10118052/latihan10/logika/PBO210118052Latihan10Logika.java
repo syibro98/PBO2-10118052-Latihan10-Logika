f9fca7225607c9b37ca6fcf5190614adcf755aa2
Bagus Syibro Malisi <55564197+syibro98@users.noreply.github.com>
2019-09-24 12:45:59 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118052/latihan10/logika/PBO210118052Latihan10Logika.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118052.latihan10.logika;
+
+/**
+ * @author 
+ * NAMA      : Bagus Syibro Malisi
+ * KELAS     : PBO2
+ * NIM       : 10118052
+ * Deskripsi Program : Program ini berisi program untuk menmapilakn data umur
+ */
+public class PBO210118052Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
